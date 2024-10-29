de39ab193b82300b78683684ca5cd2bc13b66d05
Trummer <marcusstrummer1@gmail.com>
2024-10-29 17:25:55 -0300

    🚧 WIP: adding HEAD ping

--- src/main/java/com/backend/organiza/controller/PingController.java
+++ src/main/java/com/backend/organiza/controller/PingController.java
@@ -3,17 +3,20 @@ package com.backend.organiza.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/ping")
 public class PingController {
 
     @Operation(summary = "Ping to prevent BED from sleeping")
-    @GetMapping()
+    @GetMapping("api/ping")
     public ResponseEntity<String> pingServer() {
         return ResponseEntity.status(HttpStatus.OK).body("Pong!");
+    }
+
+
+    @RequestMapping(value = "api/ping", method = RequestMethod.HEAD)
+    @ResponseStatus(HttpStatus.OK)
+    public void handleHeadPing() {
     }
 }
